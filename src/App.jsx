099d73b35acb1fc14f595f5597007ece4392d1b1
Ahmad Zaki Fitra <99023372+zachfitra@users.noreply.github.com>
2025-07-27 20:33:55 +0700

    commpit 1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <div className='wrapper'>
+      <h3 className="header">Nadisa Skincare</h3>
+      <div className='navbar'>
+      <a href="#">Home</a>
+      <a href="#">Profile</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
+
+
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
