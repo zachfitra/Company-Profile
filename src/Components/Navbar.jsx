9f62d9ca17a1d3196c9e4d39ee827703674b188d
Ahmad Zaki Fitra <99023372+zachfitra@users.noreply.github.com>
2025-08-01 15:45:56 +0700

    menambah kan icon ke navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,25 +21,29 @@ const Navbar = () => {
                     <h1 className="text-2xl font-bold">Nadisa Skincare</h1> 
                 </div>
             <ul className={`flex lg:gap-12 md:static md:bg-transparent md:w-auto md:h-full md:translate-y-0
-                md:text-black md:flex-row md:shadow-none md:p-0 md:m-0 md:transition-none gap-8 fixed ${menuActive} top-1/2 
-                flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-sky-400
-            font-bold text-white  transition-all`}>
-                        <li>
-                            <a href="#" className="font-medium opacity-75">Home</a>
+                md:text-black md:flex-row md:shadow-none md:p-0 md:m-0 md:transition-none 
+                gap-8 fixed ${menuActive} top-1/2 flex-col px-8 py-6 rounded shadow-lg
+                shadow-slate-300 bg-sky-400 font-bold text-white  transition-all`}>
+                        <li className='flex items-center gap-2 '>
+                            <i className="ri-home-2-line md:hidden block "></i>
+                            <a href="#" className="font-medium opacity-75 ">Home</a>
                         </li>
-                         <li>
+                         <li className='flex items-center gap-2'>
+                             <i className="ri-information-line md:hidden block "></i>
                             <a href="#" className= "font-medium opacity-75" >About Us</a>
                         </li>
-                         <li>
+                         <li className='flex items-center gap-2'>
+                            <i class="ri-service-line md:hidden block"></i>
                             <a href="#" className= "font-medium opacity-75">Services</a>
                         </li>
-                         <li>
+                         <li className='flex items-center gap-2'>
+                             <i class="ri-customer-service-2-line md:hidden block"></i>
                             <a href="#" className= "font-medium opacity-75"  >Contact</a>
                         </li>
                         
                     </ul>
                     <div className="social flex items-center gap-2">
-                        <a href="#" className="bg-sky-400 px-5 py-2 rounded-full text-white font-bold hover:bg-sky-500 transition-all">Social Media</a>
+                        <a href="#" className="bg-sky-400 px-5 py-2 rounded-full text-white font-bold hover:bg-sky-500 transition-all ">Social Media</a>
                         <i className="ri-menu-3-line text-3xl md:hidden block" onClick={handleClick}></i>
                     </div>
                 
